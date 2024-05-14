6b45108c521e750630a0c7ababe8851822e5d818
Muhammed Emin BARKOÇ <eminbarkoc@gmail.com>
2024-05-14 10:56:50 +0300

    resume css

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ const navToggle = document.querySelector(".navbar-toggle");
 navToggle.addEventListener("click", function(){
 document.querySelector(".portfolio-navbar").classList.toggle("show"); 
 });
+
+
+
+// Tab interface for different section of resume
+
+const resumeHeading = document.querySelector(".resume-heading");
+const resumeItems = document.querySelectorAll(".resume-item");
+const resumeTabs = document.querySelectorAll(".resume-tab");
+
+resumeHeading.onclick = (event) => {
+  event.preventDefault();
+  const clickedItemId = event.target.dataset.id;
+  if (clickedItemId) {
+    resumeItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    event.target.parentElement.classList.add("active");
+
+    resumeTabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    const correspondingTab = document.getElementById(clickedItemId);
+    correspondingTab.classList.add("active");
+  }
+};
